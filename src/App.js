f75b59505a0329cd0c0fe04e561edaece2ca675f
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-07-25 20:28:11 +0530

    Show all the items in a list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import Thumbnail from './components/Thumbnail';
+import { items, itemImages } from './items';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {items.map(({ itemId, imageId, title }) => (
+        <Thumbnail
+          key={itemId}
+          image={itemImages[imageId]}
+          title={title}
+        />
+      ))}
     </div>
   );
 }
