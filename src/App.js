91f854d010561682a6cbac8e6c6ad570c24ababb
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-07-29 14:34:42 +0530

    Fetch data from backend instead of local files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Home from './components/Home';
 import Header from './components/Header';
-import { items } from './items';
 
 const storeName = 'Grudging Sales';
 
 function App() {
+  const [items, setItems] = useState([]);
   useEffect(() => {
-    axios.get('/api/items');
+    axios.get('/api/items')
+      .then((response) => setItems(response.data))
+      .catch(console.error);
   }, []);
 
   return (
