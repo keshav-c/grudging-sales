568c02ed7d19d21a4b1d29498cb15a1726df8acc
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-08-02 16:49:45 +0530

    Add nav link to header to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import AngryLogo from '../images/logo.svg';
 import './Header.css';
 
 function Header({ title }) {
   return (
     <header className="header-component">
-      <img className="angry-logo-img" src={AngryLogo} alt="Grudging Sales" />
-      <h1>{title}</h1>
+      <Link to="/">
+        <img className="angry-logo-img" src={AngryLogo} alt="Grudging Sales" />
+        <h1>{title}</h1>
+      </Link>
     </header>
   );
 }
